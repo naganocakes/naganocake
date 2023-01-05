439c5fdf319127feb32ccf0d62188c0091535fda
EC2 Default User <ec2-user@ip-172-31-15-173.ap-northeast-1.compute.internal>
2023-01-05 15:47:10 +0000

    [Add build script Item,genre]

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :customers
+    devise_for :customers
   devise_for :users
+  namespace :admin do
+    get 'homes/top'
+    # resources :customers, only: [:index]
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :items, except: :destroy
+    # resources :orders, only: [:index, :show, :update]
+    # resources :order_details, only: [:update]
+    # get 'search' => 'items#search'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
